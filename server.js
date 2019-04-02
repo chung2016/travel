d1698915e20ff8ac1612821f9894b32409ccb9b4
chung <chung19941024@gmail.com>
2019-04-02 17:46:30 +0800

    add frontend angular

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const errorHandler = require("./api/_helpers/error_handles");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const distPath = process.env.NODE_ENV === 'production' ? '' : '/assignment';
 
+app.use(express.static(`${__dirname}/dist${distPath}`));
 app.use(cors());
 
 // use JWT auth to secure the api
@@ -15,12 +17,12 @@ app.use(jwt());
 
 // api routes
 app.use("/api/v1/users", require("./api/controllers/user.controller"));
-
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(`${__dirname}/dist${distPath}/index.html`));
+});
 // global error handler
 app.use(errorHandler);
-app.use((req, res, next) => {
-    res.status(404).send({ message: "Sorry cant find that!" });
-});
+
 // start server
 const port = process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
 app.listen(port, () => {
