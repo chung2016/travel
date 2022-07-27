3ef8426a013249430184cb8a49b394cce443ff55
Chung <chung19941024@gmail.com>
2022-07-27 10:03:46 +0800

    v2 profile observable

--- src/app/v1/shared/place-list/place-list.component.ts
+++ src/app/v1/shared/place-list/place-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input } from '@angular/core'
-import { finalize, map, reduce } from 'rxjs/operators'
+import { catchError, map, startWith } from 'rxjs/operators'
 import { PlaceListConfig } from '@v1/core/models'
 import { PlaceService } from '@v1/core/services'
-import { Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
 
 @Component({
   selector: 'app-place-list',
@@ -18,21 +18,27 @@ export class PlaceListComponent {
   }
   query: PlaceListConfig
   places$: Observable<Object>
-  loading = true
 
   private runQuery() {
     const request = this.query.filters.author
       ? this.placeService.getByUserId(this.query.filters.author)
       : this.placeService.getAll()
     this.places$ = request.pipe(
-      map((val: []) =>
-        val.reduce((acc, curr, index) => {
+      map((val: []) => ({
+        loading: false,
+        value: val.reduce((acc, curr, index) => {
           if (index % 3 === 0) acc.push([])
           acc[acc.length - 1].push(curr)
           return acc
-        }, [])
-      ),
-      finalize(() => (this.loading = false))
+        }, []),
+      })),
+      catchError((error) => {
+        return of({
+          loading: false,
+          error,
+        })
+      }),
+      startWith({ loading: true })
     )
   }
 }
